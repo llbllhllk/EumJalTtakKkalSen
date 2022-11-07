426c9800506db9daf788021876843154dbb914e1
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2022-11-07 20:44:15 +0900

    📝 Docs: Modify indentation Map.js

--- assets/js/map.js
+++ assets/js/map.js
@@ -80,44 +80,36 @@ function displayPlaces(places) {
   removeMarker();
   
   for ( var i=0; i<places.length; i++ ) {
+    // 마커를 생성하고 지도에 표시합니다
+    var placePosition = new kakao.maps.LatLng(places[i].y, places[i].x),
+      marker = addMarker(placePosition, i), 
+      itemEl = getListItem(i, places[i]); // 검색 결과 항목 Element를 생성합니다
 
-      // 마커를 생성하고 지도에 표시합니다
-      var placePosition = new kakao.maps.LatLng(places[i].y, places[i].x),
-          marker = addMarker(placePosition, i), 
-          itemEl = getListItem(i, places[i]); // 검색 결과 항목 Element를 생성합니다
+    // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+    // LatLngBounds 객체에 좌표를 추가합니다
+    bounds.extend(placePosition);
 
-      // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-      // LatLngBounds 객체에 좌표를 추가합니다
-      bounds.extend(placePosition);
+    // 마커와 검색결과 항목에 mouseover 했을때
+    // 해당 장소에 인포윈도우에 장소명을 표시합니다
+    // mouseout 했을 때는 인포윈도우를 닫습니다
+    (function(marker, title) {
+      kakao.maps.event.addListener(marker, 'mouseover', function() {
+        displayInfowindow(marker, title);
+      });
 
-      // 마커와 검색결과 항목에 mouseover 했을때
-      // 해당 장소에 인포윈도우에 장소명을 표시합니다
-      // mouseout 했을 때는 인포윈도우를 닫습니다
-      (function(marker, title) {
-          kakao.maps.event.addListener(marker, 'mouseover', function() {
-              displayInfowindow(marker, title);
-          });
-          
-          // 마커 클릭시 페이지 이동
-          kakao.maps.event.addListener(marker, 'click',
-          function() {
+      kakao.maps.event.addListener(marker, 'mouseout', function() {
+        infowindow.close();
+      });
+      
+      itemEl.onmouseover =  function () {
+        displayInfowindow(marker, title);
+      };
 
-          });
-
-          kakao.maps.event.addListener(marker, 'mouseout', function() {
-              infowindow.close();
-          });
-          
-          itemEl.onmouseover =  function () {
-              displayInfowindow(marker, title);
-          };
-
-          itemEl.onmouseout =  function () {
-              infowindow.close();
-          };
-      })(marker, places[i].place_name);
-
-      fragment.appendChild(itemEl);
+      itemEl.onmouseout =  function () {
+        infowindow.close();
+      };
+    })(marker, places[i].place_name);
+    fragment.appendChild(itemEl);
   }
 
   // 검색결과 항목들을 검색결과 목록 Element에 추가합니다`
@@ -155,28 +147,26 @@ function getListItem(index, places) {
 // 마커를 생성하고 지도 위에 마커를 표시하는 함수입니다
 function addMarker(position, idx, title) {
   var imageSrc = 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_number_blue.png', // 마커 이미지 url, 스프라이트 이미지를 씁니다
-      imageSize = new kakao.maps.Size(36, 37),  // 마커 이미지의 크기
-      imgOptions =  {
-          spriteSize : new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
-          spriteOrigin : new kakao.maps.Point(0, (idx*46)+10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
-          offset: new kakao.maps.Point(13, 37) // 마커 좌표에 일치시킬 이미지 내에서의 좌표
-      },
-      markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions),
-          marker = new kakao.maps.Marker({
-          position: position, // 마커의 위치
-          image: markerImage 
-      });
-
+    imageSize = new kakao.maps.Size(36, 37),  // 마커 이미지의 크기
+    imgOptions =  {
+      spriteSize : new kakao.maps.Size(36, 691), // 스프라이트 이미지의 크기
+      spriteOrigin : new kakao.maps.Point(0, (idx*46)+10), // 스프라이트 이미지 중 사용할 영역의 좌상단 좌표
+      offset: new kakao.maps.Point(13, 37) // 마커 좌표에 일치시킬 이미지 내에서의 좌표
+    },
+    markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imgOptions),
+      marker = new kakao.maps.Marker({
+      position: position, // 마커의 위치
+      image: markerImage 
+    });
   marker.setMap(map); // 지도 위에 마커를 표출합니다
   markers.push(marker);  // 배열에 생성된 마커를 추가합니다
-
   return marker;
 }
 
 // 지도 위에 표시되고 있는 마커를 모두 제거합니다
 function removeMarker() {
   for ( var i = 0; i < markers.length; i++ ) {
-      markers[i].setMap(null);
+    markers[i].setMap(null);
   }   
   markers = [];
 }
@@ -184,30 +174,27 @@ function removeMarker() {
 // 검색결과 목록 하단에 페이지번호를 표시는 함수입니다
 function displayPagination(pagination) {
   var paginationEl = document.getElementById('pagination'),
-      fragment = document.createDocumentFragment(),
-      i; 
-
+    fragment = document.createDocumentFragment(),
+    i;
   // 기존에 추가된 페이지번호를 삭제합니다
   while (paginationEl.hasChildNodes()) {
-      paginationEl.removeChild (paginationEl.lastChild);
+    paginationEl.removeChild (paginationEl.lastChild);
   }
 
   for (i=1; i<=pagination.last; i++) {
-      var el = document.createElement('a');
-      el.href = "#";
-      el.innerHTML = i;
-
-      if (i===pagination.current) {
-          el.className = 'on';
-      } else {
-          el.onclick = (function(i) {
-              return function() {
-                  pagination.gotoPage(i);
-              }
-          })(i);
-      }
-
-      fragment.appendChild(el);
+    var el = document.createElement('a');
+    el.href = "#";
+    el.innerHTML = i;
+    if (i===pagination.current) {
+      el.className = 'on';
+    } else {
+      el.onclick = (function(i) {
+        return function() {
+          pagination.gotoPage(i);
+        }
+      })(i);
+    }
+    fragment.appendChild(el);
   }
   paginationEl.appendChild(fragment);
 }
@@ -223,6 +210,6 @@ function displayInfowindow(marker, title) {
 // 검색결과 목록의 자식 Element를 제거하는 함수입니다
 function removeAllChildNods(el) {   
   while (el.hasChildNodes()) {
-      el.removeChild (el.lastChild);
+    el.removeChild (el.lastChild);
   }
 }
